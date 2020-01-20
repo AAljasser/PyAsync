8dea1bd0474ea055f95fd12ffd990865b7adc2a8
unknown <abdullrhmanaljasser@gmail.com>
2020-01-20 15:09:53 -0500

    Library Creation complete

--- test/libraryUnitTest.py
+++ test/libraryUnitTest.py
@@ -1,12 +1,13 @@
 import unittest
-from main import library
+from main.Library import Library
 
 
 class TestingLibrarySystem(unittest.TestCase):
 
     #Definition of a library
     def test_libDef(self):
-        self.assertIsInstance(library(),type(library()))
+        x = Library()
+        self.assertTrue(type(x) is Library)
 
 if __name__== '__main__':
     unittest.main()
