3babd96599cb7e6d0457c89869e2af4880145337
Abdullrhman Aljasser <abdullrhmanaljasser@WINDOWS2.SCS.CARLETON.CA>
2020-01-22 15:51:24 -0500

    Code added - Modification to Library for singleton

--- main/Library.py
+++ main/Library.py
@@ -1,5 +1,20 @@
-class Library:
+class Library(object):
+    class __singleInstance:
+        def __init__(self):
+            #Singleton variables
+            self.att = None
 
 
-    def __init__(self):
-        self = self;
+    #Public library attributes
+    instance = None
+
+    #Python provide __NEW__ to allow for singleton class object
+    def __new__(cls):
+        if not Library.instance:
+            Library.instance = Library.__singleInstance()
+        return Library.instance
+    #This will allow us to modify singleton attributes
+    def __getattr__(self, item):
+        return getattr(self.instance,item)
+    def __setattr__(self, item):
+        return getattr(self.instance,item)
