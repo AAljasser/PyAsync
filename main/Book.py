4191e38e81d6e8eef039fc070670c5c71d5728e5
Abdullrhman Aljasser <abdullrhmanaljasser@gmail.com>
2020-02-12 17:35:26 -0500

    Code - Book Class

---
+++ main/Book.py
@@ -0,0 +1,11 @@
+class Book():
+    _id = None
+    _title = None
+
+    def __init__(self,title,id):
+        self._id = id
+        self._title
+    def get_id(self):
+        return self._id
+    def get_title(self):
+        return self._title
