317283a0f1d374ae034dc632576434296184f08c
unknown <abdullrhmanaljasser@gmail.com>
2020-02-20 14:22:33 -0500

    Code Commit - Patreon  book borrow added

--- main/Patreon.py
+++ main/Patreon.py
@@ -3,7 +3,7 @@
 class Patreon():
     _pid = None
     _pname = None
-    _bCollection = [] # List of books
+    _bCollection = {} # Dic
     def __init__(self,name,id):
         self._pname = name
         self._pid = id
@@ -11,4 +11,7 @@ class Patreon():
         return self._pid
     def get_name(self):
         return self._pname
-
+    def addBook(self,bid,bobj):
+        ##FOR Now only add books that do not exists already, since duplicate key could cause latter issue
+        if bid not in self._bCollection.keys():
+            self._bCollection[bid] = bobj
